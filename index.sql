1876b98ccc8d86d92acb3265fc2f9d593456aa99
Jeffrey Chi <thejeffreychi@gmail.com>
2022-05-08 00:28:51 -0700

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,6 +1,12 @@
--- SELECT 
---   title,
---   score
--- FROM hacker_news
--- ORDER BY score DESC
--- LIMIT 5;
+SELECT 
+  title,
+  score
+FROM hacker_news
+ORDER BY score DESC
+LIMIT 5;
+
+-- total score of all stories
+SELECT
+  SUM(score) AS total_score
+FROM hacker_news
+ORDER BY score DESC;
