5603959dbb9e1225678fde2ff412746869cce948
Jeffrey Chi <thejeffreychi@gmail.com>
2022-05-08 00:30:24 -0700

    Update index.sql

--- index.sql
+++ index.sql
@@ -10,3 +10,12 @@ SELECT
   SUM(score) AS total_score
 FROM hacker_news
 ORDER BY score DESC;
+
+--  total combined score over 200 in DESC
+SELECT 
+  title,
+  SUM(score) AS total_score
+FROM hacker_news
+GROUP BY title
+HAVING SUM(score) > 200
+ORDER BY total_score DESC;
